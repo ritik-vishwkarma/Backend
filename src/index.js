eb51acfab558996b5412132a892f73d4490979cc
ritik-vishwkarma <vishwakarmaritik1563@gamil.com>
2024-03-17 18:05:53 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+/*
 import mongoose from "mongoose"
 import { DB_NAME } from "./constants";
-/*
 import express from "express";
 const app = express()
 
