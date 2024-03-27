dedda6c6151ca32d538916a812f9d968e8cefe6e
ritik-vishwkarma <vishwakarmaritik1563@gamil.com>
2024-03-27 21:03:20 +0530

    multer and cloudinary files added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null
+    //upload the file on cloudinary
+    const respose = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto"
+    })
+    // file has been uploaded successfull
+    console.log("File is Uploaded on Cloudinary", respose);
+    return response;
+
+  } catch (error) {
+    fs.unlinkSync(loaclFilePath) // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+}
+
+export { uploadOnCloudinary }
