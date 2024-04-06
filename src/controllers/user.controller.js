844f8d2955c0384ee36b7d9013dde6cf669a27b4
ritik-vishwkarma <vishwakarmaritik1563@gamil.com>
2024-04-06 12:12:28 +0530

    Some bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -250,7 +250,7 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 const getCurrentUser = asyncHandler(async(req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "Current user fetched successfully")
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res) => {
@@ -260,7 +260,7 @@ const updateAccountDetails = asyncHandler(async(req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
@@ -282,6 +282,8 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
     if (!avatorLocalPath) {
         throw new ApiError(400, "Avator is missing")
     }
+
+    // TODO: delete old image - assignment
 
     const avatar = await uploadOnCloudinary(avatorLocalPath)
 
